d8f24b9a52d3c3ba62ab3796c5de161eb0691a88
SaTsSo <stsai30160@ufide.ac.cr>
2026-06-12 07:30:54 -0600

    Se instancian los planetas Mercurio y Venus. Se crea un lista para los planetas. Y se genera un bucle para imprimir los detalles de los planetas.

--- sistema_solar/src/main/java/Main.java
+++ sistema_solar/src/main/java/Main.java
@@ -1,7 +1,23 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("probando ´prbad");
-        Planeta[] planetas = new Planeta[8];
+
+        //Instanciar planetas
+        Planeta mercurio = new PlanetaRocoso("Mercurio", 4.879, 57.9, 0);
+        Planeta venus = new PlanetaRocoso("Venus", 12.104, 108.2, 0);
+
+        //Crear lista de planetas
+        List<Planeta> planetas = new ArrayList<>();
+        planetas.add(mercurio);
+        planetas.add(venus);
+
+        //Imprimir los detalles de los planetas
+        for (Planeta planeta : planetas){
+            System.out.println(planeta.toString());
+        }
     }
 }
